7af89c4318de1824864de5af698b6187ab7a9864
Chris Johnston <cjohnston@connected.io>
2022-10-13 11:20:54 -0400

    adds approvals

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source "https://rubygems.org"
 # Installing both. Why choose??
 
 gem "rspec"
+gem 'rake'
+gem 'approvals', '~> 0.0.26'
---
+++ spec/ugly_trivia/game_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require './lib/ugly_trivia/game'
+
+describe UglyTrivia::Game do
+  it 'should work' do
+    expect(true).to eq(true)
+  end
+end
